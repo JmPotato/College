817d0b4ccb6890dc92427d68c2ae633de5821e17
JmPotato <ghzpotato@gmail.com>
2014-07-16 14:04:47 +0800

    account handler model

--- handler/account.py
+++ handler/account.py
@@ -1,3 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
+import hashlib
+import tornado.web
+
+from . import BaseHandler
+
+class SignupHandler(BaseHandler):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class SigninHandler(BaseHandler):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class SignoutHandler(BaseHandler):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class SettingsHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        pass
+
+    @tornado.web.authenticated
+    def post(self):
+        pass
