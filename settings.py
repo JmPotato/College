4b24977152a9b63a220d42ecdc3af177c1b996af
JmPotato <ghzpotato@gmail.com>
2014-07-22 22:24:39 +0800

    Other update

--- settings.py
+++ settings.py
@@ -8,15 +8,15 @@ site_url = 'http://xxx.com/'
 google_analytics = ''
 cookie_secret = 'RESET ME!!!!'
 
-# MongoDB Settings
-mongodb_host = '127.0.0.1'
-mongodb_port = 27017
-database_name = 'college'
-
 # Qiniu Settings
 qiniu_access_key = ""
 qiniu_secret_key = ""
 qiniu_bucket_name = ""
+
+# MongoDB Settings
+mongodb_host = '127.0.0.1'
+mongodb_port = 27017
+database_name = 'college'
 
 # Development Settings
 Debug = True
